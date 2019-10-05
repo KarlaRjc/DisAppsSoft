419b12df4067168e7f4e0870aab33ef29401a8b2
Christian Hardin Rodríguez <chardinr840@ulacit.ed.cr>
2019-10-05 15:48:16 -0600

    Polimorfismo entre users y admin, teacher, gatherer

---
+++ AssetsManagement/AssetsManagement/Admin.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetsManagement
+{
+    public class Admin : Users
+    {
+        public void CreateUser()
+        {
+
+        }
+
+        public void EditUser()
+        {
+
+        }
+
+        public void ChangePassword()
+        {
+
+        }
+
+        public void GetUserById()
+        {
+
+        }
+
+        public void DeleteUser()
+        {
+
+        }
+    }
+}

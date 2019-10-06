a873b8e86079a21ba96a8ecf884428e77b9a96bf
Alexander <alexgomezde@gmail.com>
2019-10-05 23:20:14 -0600

    EnumRole and Util class added

---
+++ AssetsManagement/AssetsManagement/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetsManagement
+{
+    public static class Utils
+    {
+        public string GetUserRole()
+        {
+
+        }
+
+        public boolean IsUserTeacher()
+        {
+               
+        }
+
+        public boolean IsUserAdmin()
+        {
+
+        }
+
+        public boolean IsUserGatherer()
+        {
+
+        }
+
+
+    }
+}

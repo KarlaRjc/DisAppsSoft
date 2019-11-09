16c3ba3929a33ff51cc2d9a3b2e47573dda6056a
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-11-09 11:52:50 -0600

    delelted buttons

--- AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
+++ AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
@@ -30,25 +30,21 @@
         {
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel2 = new System.Windows.Forms.Panel();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.panel3 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button6 = new System.Windows.Forms.Button();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.label7 = new System.Windows.Forms.Label();
             this.label8 = new System.Windows.Forms.Label();
-            this.button7 = new System.Windows.Forms.Button();
-            this.button8 = new System.Windows.Forms.Button();
-            this.button9 = new System.Windows.Forms.Button();
-            this.button10 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
             this.panel3.SuspendLayout();
@@ -57,10 +53,6 @@
             // panel1
             // 
             this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(164)))), ((int)(((byte)(191)))));
-            this.panel1.Controls.Add(this.button10);
-            this.panel1.Controls.Add(this.button9);
-            this.panel1.Controls.Add(this.button8);
-            this.panel1.Controls.Add(this.button7);
             this.panel1.Controls.Add(this.panel2);
             this.panel1.Controls.Add(this.panel3);
             this.panel1.Location = new System.Drawing.Point(1, 2);
@@ -78,6 +70,17 @@
             this.panel2.Size = new System.Drawing.Size(1280, 70);
             this.panel2.TabIndex = 0;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(1144, 7);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(49, 48);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Hola\r\nUser";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
@@ -89,17 +92,6 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Manejo de Activos";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.White;
-            this.label2.Location = new System.Drawing.Point(1144, 7);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(49, 48);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Hola\r\nUser";
             // 
             // panel3
             // 
@@ -121,103 +113,16 @@
             this.panel3.Size = new System.Drawing.Size(247, 741);
             this.panel3.TabIndex = 1;
             // 
-            // button1
+            // label8
             // 
-            this.button1.Location = new System.Drawing.Point(13, 28);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 75);
-            this.button1.TabIndex = 2;
-            this.button1.Text = "computer";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(11, 123);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(85, 78);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "user";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(13, 226);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(83, 73);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "transference";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button4
-            // 
-            this.button4.Location = new System.Drawing.Point(11, 324);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(83, 71);
-            this.button4.TabIndex = 4;
-            this.button4.Text = "transfer history";
-            this.button4.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(11, 421);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(83, 70);
-            this.button5.TabIndex = 2;
-            this.button5.Text = "Reports";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button6
-            // 
-            this.button6.Location = new System.Drawing.Point(11, 584);
-            this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(66, 38);
-            this.button6.TabIndex = 5;
-            this.button6.Text = "exit";
-            this.button6.UseVisualStyleBackColor = true;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.White;
-            this.label3.Location = new System.Drawing.Point(99, 54);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(138, 20);
-            this.label3.TabIndex = 6;
-            this.label3.Text = "Manejo de Activos";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.ForeColor = System.Drawing.Color.White;
-            this.label4.Location = new System.Drawing.Point(99, 151);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(150, 20);
-            this.label4.TabIndex = 2;
-            this.label4.Text = "Manejo de Usuarios";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(102, 237);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(106, 40);
-            this.label5.TabIndex = 2;
-            this.label5.Text = "Realizar\r\nTransferencia";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(102, 335);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(114, 40);
-            this.label6.TabIndex = 2;
-            this.label6.Text = "Historial de\r\nTransferencias";
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(83, 602);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(40, 20);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Salir";
             // 
             // label7
             // 
@@ -230,65 +135,103 @@
             this.label7.TabIndex = 2;
             this.label7.Text = "Reportes";
             // 
-            // label8
+            // label6
             // 
-            this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label8.ForeColor = System.Drawing.Color.White;
-            this.label8.Location = new System.Drawing.Point(83, 602);
-            this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(40, 20);
-            this.label8.TabIndex = 7;
-            this.label8.Text = "Salir";
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(102, 335);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 40);
+            this.label6.TabIndex = 2;
+            this.label6.Text = "Historial de\r\nTransferencias";
             // 
-            // button7
+            // label5
             // 
-            this.button7.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(63)))), ((int)(((byte)(140)))));
-            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button7.ForeColor = System.Drawing.Color.White;
-            this.button7.Location = new System.Drawing.Point(803, 303);
-            this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(130, 44);
-            this.button7.TabIndex = 2;
-            this.button7.Text = "EDITAR";
-            this.button7.UseVisualStyleBackColor = false;
-            this.button7.Click += new System.EventHandler(this.button7_Click);
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(102, 237);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 40);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Realizar\r\nTransferencia";
             // 
-            // button8
+            // label4
             // 
-            this.button8.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(63)))), ((int)(((byte)(140)))));
-            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button8.ForeColor = System.Drawing.Color.White;
-            this.button8.Location = new System.Drawing.Point(803, 400);
-            this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(130, 40);
-            this.button8.TabIndex = 3;
-            this.button8.Text = "AGREGAR";
-            this.button8.UseVisualStyleBackColor = false;
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(99, 151);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(150, 20);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Manejo de Usuarios";
             // 
-            // button9
+            // label3
             // 
-            this.button9.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(63)))), ((int)(((byte)(140)))));
-            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button9.ForeColor = System.Drawing.Color.White;
-            this.button9.Location = new System.Drawing.Point(803, 486);
-            this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(130, 44);
-            this.button9.TabIndex = 4;
-            this.button9.Text = "ELIMINAR";
-            this.button9.UseVisualStyleBackColor = false;
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(99, 54);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(138, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Manejo de Activos";
             // 
-            // button10
+            // button6
             // 
-            this.button10.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(73)))), ((int)(((byte)(63)))), ((int)(((byte)(140)))));
-            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button10.ForeColor = System.Drawing.Color.White;
-            this.button10.Location = new System.Drawing.Point(791, 554);
-            this.button10.Name = "button10";
-            this.button10.Size = new System.Drawing.Size(142, 49);
-            this.button10.TabIndex = 5;
-            this.button10.Text = "TRANSFERIR";
-            this.button10.UseVisualStyleBackColor = false;
+            this.button6.Location = new System.Drawing.Point(11, 584);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(66, 38);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "exit";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(11, 421);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(83, 70);
+            this.button5.TabIndex = 2;
+            this.button5.Text = "Reports";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(11, 324);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(83, 71);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "transfer history";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(13, 226);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(83, 73);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "transference";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(11, 123);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(85, 78);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "user";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 28);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(83, 75);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "computer";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
@@ -326,10 +269,6 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Label label8;
-        private System.Windows.Forms.Button button7;
-        private System.Windows.Forms.Button button10;
-        private System.Windows.Forms.Button button9;
-        private System.Windows.Forms.Button button8;
     }
 }
 

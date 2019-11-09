82e5d964d42711861b4fd80908125337d0ec5684
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-11-09 13:18:24 -0600

    added into resources images

--- AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
+++ AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
@@ -91,7 +91,7 @@
             this.label1.Size = new System.Drawing.Size(165, 24);
             this.label1.TabIndex = 0;
             this.label1.Text = "Manejo de Activos";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Click += new System.EventHandler(this.Label1_Click);
             // 
             // panel3
             // 

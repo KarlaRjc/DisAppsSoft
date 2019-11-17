2e556f1c2830a6d9bd6fc635e140064b34004b37
Christian Hardin Rodríguez <chardinr840@ulacit.ed.cr>
2019-11-17 14:19:49 -0600

    Codigo para crear, modificar, eliminar usuarios

--- AssetsManagement/AssetsManagement/User.cs
+++ AssetsManagement/AssetsManagement/User.cs
@@ -23,5 +23,73 @@ namespace AssetsManagement
         {
             return null;
         }
+
+        public bool GetUserRole(UserEntity user)
+        {
+            if (user.role.ToString() == "Admin")
+            {
+                return true;
+            }
+            else return false;
+        }
+
+        public void CreateUserToDB(UserEntity user)
+        {
+            using (DbModel db = new DbModel())
+            {
+                db.User.Add(user);
+                db.SaveChanges();
+            }
+        }
+
+        public UserEntity CreateUser(int id, string name, EnumRole role, string username, string password)
+        {
+            UserEntity user = new UserEntity();
+            user.id = id;
+            user.name = name;
+            user.role = role;
+            user.username = username;
+            user.password = password;
+
+            return user;
+        }
+
+        public void ModifyUserToDB(UserEntity user)
+        {
+            //Preguntar como se usa el using
+
+            using (DbModel db = new DbModel())
+            {
+                db.Entry(user).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+            }
+        }
+
+        public UserEntity ModifyUser(int id, DbModel db)
+        {
+            UserEntity user = db.User.Find(id);
+
+            //Ver como vamos a manejar la parte de modificar
+
+            user.name = "Nuevo Nombre";
+
+            return user;
+        }
+
+        public void DeleteUserToDB(UserEntity user, DbModel db)
+        {
+            using (db)
+            {
+                db.User.Remove(user);
+                db.SaveChanges();
+            }
+        }
+
+        public UserEntity DeleteUser(User us, DbModel db)
+        {
+            UserEntity user = db.User.Find(us.id);
+
+            return user;
+        }
     }
 }

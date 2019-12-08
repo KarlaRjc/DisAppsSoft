79dbcd515a63dd4dc264f147df82b58e37e20b8c
Christian Hardin Rodríguez <chardinr840@ulacit.ed.cr>
2019-12-08 09:09:47 -0600

    Added Patron Iterator

---
+++ AssetsManagement/AssetsManagement/Model/IteratorAssetEntity.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetsManagement
+{
+    public class IteratorAssetEntity : IIteratorAssetEntity
+    {
+        private List<AssetEntity> assetEntities;
+
+        private int position = -1;
+
+        public IteratorAssetEntity(List<AssetEntity> assets)
+        {
+            this.assetEntities = assets;
+        }
+
+        public void FirstAsset()
+        {
+            this.position = -1;
+        }
+
+        public AssetEntity Actual()
+        {
+            if ((this.assetEntities == null) || (this.assetEntities.Count == 0) || (this.position < 0)){
+                return null;
+            }
+            else
+            {
+                return (AssetEntity)this.assetEntities[position];
+            }
+        }
+
+        public AssetEntity Next()
+        {
+            if ((this.assetEntities == null) || (this.assetEntities.Count == 0) || (position + 1 > this.assetEntities.Count - 1))
+            {
+                return null;
+            }
+            else
+            {
+                return (AssetEntity)this.assetEntities[++position];
+            }  
+        }
+
+        public bool ElementsLeft()
+        {
+            return (position + 1 <= this.assetEntities.Count - 1);
+        }
+    }
+}

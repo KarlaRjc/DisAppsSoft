26dbc54ed3be567198004a37bb3be44048161b39
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-12-15 15:20:47 -0600

    changed form's header description

--- AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
+++ AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
@@ -189,7 +189,7 @@
             // homePanel
             // 
             this.homePanel.Location = new System.Drawing.Point(245, 67);
-            this.homePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.homePanel.Margin = new System.Windows.Forms.Padding(2);
             this.homePanel.Name = "homePanel";
             this.homePanel.Size = new System.Drawing.Size(909, 643);
             this.homePanel.TabIndex = 2;
@@ -620,7 +620,7 @@
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "CTP Mario Quiros Sasso";
+            this.Text = "CTP Mario Quirós Sasso";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);

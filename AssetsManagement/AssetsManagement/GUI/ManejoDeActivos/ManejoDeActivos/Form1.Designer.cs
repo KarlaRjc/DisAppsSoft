18d346b5fdc21ef0403ea6539149e7a61aae3981
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-12-14 21:43:29 -0600

    Changed form´s icon and test of the banner

--- AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
+++ AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.panel1 = new System.Windows.Forms.Panel();
             this.panel3 = new System.Windows.Forms.Panel();
             this.transferHisotryBtn = new System.Windows.Forms.Button();
@@ -188,7 +189,7 @@
             // homePanel
             // 
             this.homePanel.Location = new System.Drawing.Point(245, 67);
-            this.homePanel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.homePanel.Margin = new System.Windows.Forms.Padding(2);
             this.homePanel.Name = "homePanel";
             this.homePanel.Size = new System.Drawing.Size(909, 643);
             this.homePanel.TabIndex = 2;
@@ -617,8 +618,9 @@
             this.Controls.Add(this.LoginPanel);
             this.Controls.Add(this.panel2);
             this.Controls.Add(this.panel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "CTP Mario Quiros Sasso";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.panel1.ResumeLayout(false);
             this.panel3.ResumeLayout(false);

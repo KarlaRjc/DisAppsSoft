04e3eb466796b218cb2025ec4a00c51a1a1f8298
eviltwin666 <kcamposr021@ulacit.ed.cr>
2019-12-15 11:23:41 -0600

    blocked combo box "laboratorio "for changes

--- AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/TransferAssest.Designer.cs
+++ AssetsManagement/AssetsManagement/GUI/ManejoDeActivos/ManejoDeActivos/TransferAssest.Designer.cs
@@ -30,24 +30,24 @@
         {
             this.components = new System.ComponentModel.Container();
             this.assetsTransferTable = new System.Windows.Forms.DataGridView();
+            this.Serie = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.assetEntitiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this._AssetsManagement_DbModelDataSet3 = new ManejoDeActivos._AssetsManagement_DbModelDataSet3();
             this.assetTransferHistoryEntitiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this._AssetsManagement_DbModelDataSet2 = new ManejoDeActivos._AssetsManagement_DbModelDataSet2();
             this.label1 = new System.Windows.Forms.Label();
             this.labCbx = new System.Windows.Forms.ComboBox();
             this.TransferAssetBtn = new System.Windows.Forms.Button();
             this.assetTransferHistoryEntitiesTableAdapter = new ManejoDeActivos._AssetsManagement_DbModelDataSet2TableAdapters.AssetTransferHistoryEntitiesTableAdapter();
-            this._AssetsManagement_DbModelDataSet3 = new ManejoDeActivos._AssetsManagement_DbModelDataSet3();
-            this.assetEntitiesBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.assetEntitiesTableAdapter = new ManejoDeActivos._AssetsManagement_DbModelDataSet3TableAdapters.AssetEntitiesTableAdapter();
-            this.Serie = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.descriptionDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Marca = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Modelo = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.assetsTransferTable)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetEntitiesBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.assetTransferHistoryEntitiesBindingSource)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet3)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.assetEntitiesBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // assetsTransferTable
@@ -61,84 +61,14 @@
             this.Marca,
             this.Modelo});
             this.assetsTransferTable.DataSource = this.assetEntitiesBindingSource;
-            this.assetsTransferTable.Location = new System.Drawing.Point(76, 111);
-            this.assetsTransferTable.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.assetsTransferTable.Location = new System.Drawing.Point(57, 90);
+            this.assetsTransferTable.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.assetsTransferTable.Name = "assetsTransferTable";
             this.assetsTransferTable.ReadOnly = true;
             this.assetsTransferTable.RowHeadersWidth = 51;
             this.assetsTransferTable.RowTemplate.Height = 24;
-            this.assetsTransferTable.Size = new System.Drawing.Size(672, 395);
+            this.assetsTransferTable.Size = new System.Drawing.Size(504, 321);
             this.assetsTransferTable.TabIndex = 0;
-            // 
-            // assetTransferHistoryEntitiesBindingSource
-            // 
-            this.assetTransferHistoryEntitiesBindingSource.DataMember = "AssetTransferHistoryEntities";
-            this.assetTransferHistoryEntitiesBindingSource.DataSource = this._AssetsManagement_DbModelDataSet2;
-            // 
-            // _AssetsManagement_DbModelDataSet2
-            // 
-            this._AssetsManagement_DbModelDataSet2.DataSetName = "_AssetsManagement_DbModelDataSet2";
-            this._AssetsManagement_DbModelDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(843, 155);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(110, 25);
-            this.label1.TabIndex = 7;
-            this.label1.Text = "Laboratorio";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // labCbx
-            // 
-            this.labCbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.labCbx.FormattingEnabled = true;
-            this.labCbx.Items.AddRange(new object[] {
-            "1",
-            "2",
-            "3",
-            "4",
-            "5",
-            "6"});
-            this.labCbx.Location = new System.Drawing.Point(841, 192);
-            this.labCbx.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.labCbx.Name = "labCbx";
-            this.labCbx.Size = new System.Drawing.Size(117, 33);
-            this.labCbx.TabIndex = 8;
-            // 
-            // TransferAssetBtn
-            // 
-            this.TransferAssetBtn.FlatAppearance.BorderSize = 0;
-            this.TransferAssetBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.TransferAssetBtn.Image = global::ManejoDeActivos.Properties.Resources.TransferirBtn;
-            this.TransferAssetBtn.Location = new System.Drawing.Point(807, 249);
-            this.TransferAssetBtn.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.TransferAssetBtn.Name = "TransferAssetBtn";
-            this.TransferAssetBtn.Size = new System.Drawing.Size(184, 65);
-            this.TransferAssetBtn.TabIndex = 13;
-            this.TransferAssetBtn.UseVisualStyleBackColor = true;
-            this.TransferAssetBtn.Click += new System.EventHandler(this.TransferAssetBtn_Click);
-            // 
-            // assetTransferHistoryEntitiesTableAdapter
-            // 
-            this.assetTransferHistoryEntitiesTableAdapter.ClearBeforeFill = true;
-            // 
-            // _AssetsManagement_DbModelDataSet3
-            // 
-            this._AssetsManagement_DbModelDataSet3.DataSetName = "_AssetsManagement_DbModelDataSet3";
-            this._AssetsManagement_DbModelDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
-            // 
-            // assetEntitiesBindingSource
-            // 
-            this.assetEntitiesBindingSource.DataMember = "AssetEntities";
-            this.assetEntitiesBindingSource.DataSource = this._AssetsManagement_DbModelDataSet3;
-            // 
-            // assetEntitiesTableAdapter
-            // 
-            this.assetEntitiesTableAdapter.ClearBeforeFill = true;
             // 
             // Serie
             // 
@@ -176,26 +106,98 @@
             this.Modelo.ReadOnly = true;
             this.Modelo.Width = 125;
             // 
+            // assetEntitiesBindingSource
+            // 
+            this.assetEntitiesBindingSource.DataMember = "AssetEntities";
+            this.assetEntitiesBindingSource.DataSource = this._AssetsManagement_DbModelDataSet3;
+            // 
+            // _AssetsManagement_DbModelDataSet3
+            // 
+            this._AssetsManagement_DbModelDataSet3.DataSetName = "_AssetsManagement_DbModelDataSet3";
+            this._AssetsManagement_DbModelDataSet3.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // assetTransferHistoryEntitiesBindingSource
+            // 
+            this.assetTransferHistoryEntitiesBindingSource.DataMember = "AssetTransferHistoryEntities";
+            this.assetTransferHistoryEntitiesBindingSource.DataSource = this._AssetsManagement_DbModelDataSet2;
+            // 
+            // _AssetsManagement_DbModelDataSet2
+            // 
+            this._AssetsManagement_DbModelDataSet2.DataSetName = "_AssetsManagement_DbModelDataSet2";
+            this._AssetsManagement_DbModelDataSet2.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(632, 126);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Laboratorio";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labCbx
+            // 
+            this.labCbx.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.labCbx.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labCbx.FormattingEnabled = true;
+            this.labCbx.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6"});
+            this.labCbx.Location = new System.Drawing.Point(631, 156);
+            this.labCbx.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.labCbx.Name = "labCbx";
+            this.labCbx.Size = new System.Drawing.Size(89, 28);
+            this.labCbx.TabIndex = 8;
+            // 
+            // TransferAssetBtn
+            // 
+            this.TransferAssetBtn.FlatAppearance.BorderSize = 0;
+            this.TransferAssetBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.TransferAssetBtn.Image = global::ManejoDeActivos.Properties.Resources.TransferirBtn;
+            this.TransferAssetBtn.Location = new System.Drawing.Point(605, 202);
+            this.TransferAssetBtn.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.TransferAssetBtn.Name = "TransferAssetBtn";
+            this.TransferAssetBtn.Size = new System.Drawing.Size(138, 53);
+            this.TransferAssetBtn.TabIndex = 13;
+            this.TransferAssetBtn.UseVisualStyleBackColor = true;
+            this.TransferAssetBtn.Click += new System.EventHandler(this.TransferAssetBtn_Click);
+            // 
+            // assetTransferHistoryEntitiesTableAdapter
+            // 
+            this.assetTransferHistoryEntitiesTableAdapter.ClearBeforeFill = true;
+            // 
+            // assetEntitiesTableAdapter
+            // 
+            this.assetEntitiesTableAdapter.ClearBeforeFill = true;
+            // 
             // TransferAssest
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(163)))), ((int)(((byte)(164)))), ((int)(((byte)(191)))));
-            this.ClientSize = new System.Drawing.Size(1195, 745);
+            this.ClientSize = new System.Drawing.Size(896, 605);
             this.Controls.Add(this.TransferAssetBtn);
             this.Controls.Add(this.labCbx);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.assetsTransferTable);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "TransferAssest";
             this.Text = "TransferAssest";
             this.Load += new System.EventHandler(this.TransferAssest_Load);
             ((System.ComponentModel.ISupportInitialize)(this.assetsTransferTable)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.assetEntitiesBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet3)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.assetTransferHistoryEntitiesBindingSource)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this._AssetsManagement_DbModelDataSet3)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.assetEntitiesBindingSource)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
